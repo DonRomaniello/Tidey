de8dbf8885f98d1714443935d456ce3fb60a9133
Don Romaniello <don.romaniello@gmail.com>
2022-07-30 18:35:39 -0400

    Install axios, note tide API data.

---
+++ src/components/NOAAListCall.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+
+// Master list
+// https://api.tidesandcurrents.noaa.gov/mdapi/prod/
+
+// Format
+// https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/stations/9455760.json?units=english
