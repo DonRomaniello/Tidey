5e34fea394132df3a2d78b38f555b4960a4812ab
Don Romaniello <don.romaniello@gmail.com>
2022-07-30 20:09:20 -0400

    Get station list from NOAA API.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
+import axios from 'axios';
+
+import React, {useEffect, useState} from 'react';
+
 import logo from './logo.svg';
+
 
 import './App.css';
 
 import ReactLeaflet from './components/reactLeaflet';
 
+import getStations from './components/NOAAListCall';
+
 function App() {
 
-  const mapDivStyle = {
-    display: 'flex',
-    height:'100vh',
-    justifyContent: 'center',
-    alignItems: 'center',
-    // transform: 'rotate3d(1, 0, 0, 45deg)',
-  }
+  const [stations, setStations] = useState([])
+
+  useEffect(() => {
+
+    let url = 'https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/portsstation.json'
+
+    axios.get(url)
+    .then(response => {
+      const stationData = response.data
+      setStations(stationData?.portsStationList)
+    })
+
+  }, [])
+
 
   return (
     <div style={mapDivStyle}
     >
-      <ReactLeaflet />
+      <ReactLeaflet stations={stations} />
     </div>
   );
 }
 
 export default App;
+
+const mapDivStyle = {
+  display: 'flex',
+  height:'100vh',
+  justifyContent: 'center',
+  alignItems: 'center',
+  // transform: 'rotate3d(1, 0, 0, 45deg)',
+}
--- src/components/NOAAListCall.js
+++ src/components/NOAAListCall.js
@@ -1,8 +1,27 @@
-import axios from "axios";
-
+const axios =  require("axios");
 
 // Master list
 // https://api.tidesandcurrents.noaa.gov/mdapi/prod/
 
 // Format
 // https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/stations/9455760.json?units=english
+
+
+
+const getStations =  async () => {
+
+  let stations = await axios.get('https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/portsstation.json')
+  .then(function (response) {
+    // handle success
+    return response.data;
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+
+  return stations
+
+}
+
+export default getStations
