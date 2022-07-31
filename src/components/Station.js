9ee26a053371ab44e553b773d68f8e1aaf6ff0fd
Don Romaniello <don.romaniello@gmail.com>
2022-07-30 20:26:38 -0400

    Sort out the cicle getting clipped.

--- src/components/Station.js
+++ src/components/Station.js
@@ -11,9 +11,10 @@ import {
 const Station = (props) => {
 
 const position = [40.778041, -73.921264]
+
 const bounds = [
   position,
-  [position[0] - .2, position[1] - .1],
+  [position[0] - 1, position[1] + 1],
 ]
 
 const overlayStyle = {
@@ -21,12 +22,18 @@ const overlayStyle = {
   transform: 'rotate3d(1, 0, 0, 45deg)',
 }
 
+const circleRadius = 10
+
 
   return (
     <>
     <SVGOverlay attributes={{ stroke: 'red' }} bounds={bounds} style={{overlayStyle}}>
       {/* <rect x="0" y="0" width="100%" height="100%" fill="blue" opacity='.5' /> */}
-      <circle r="5" cx="10" cy="10" fill="red" opacity='1' />
+      <circle r={circleRadius}
+      cx={circleRadius + 1}
+      cy={circleRadius + 1}
+      fill="red"
+      opacity='1' />
       {/* <text x="50%" y="50%" stroke="white" opacity='.5'>
         text
       </text> */}
