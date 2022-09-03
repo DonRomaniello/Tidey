c3eaa121ecdb553aeef1cd04c812b36e6b033145
Don Romaniello <don.romaniello@gmail.com>
2022-09-03 06:19:36 -0400

    Hover toggle.

--- src/components/CompassRose.js
+++ src/components/CompassRose.js
@@ -1,6 +1,6 @@
 
 
-import React from 'react';
+import React, {useState} from 'react';
 
 
 import { ReactComponent as CompassZeroLayer} from './assets/Compass_0Layer.svg'
@@ -15,13 +15,27 @@ import styles from './css/CompassRose.module.css'
 
 const CompassRose = () => {
 
+  const [compassHover, setCompassHover] = useState(false);
+
+
+  const hoverClass = (layerClass) => {
+    if (compassHover) {
+      return styles[layerClass + 'Hover']
+    }
+  }
+
   return (
     <>
-    <div id={styles.compassHolder} className={styles.compass}>
-      <CompassTopLayer className={`${styles.topLayer} + ${styles.compass}`}/>
-      <CompassTwoLayer className={`${styles.twoLayer} + ${styles.compass}`}/>
-      <CompassOneLayer className={`${styles.oneLayer} + ${styles.compass}`}/>
-      <CompassZeroLayer className={`${styles.zeroLayer} + ${styles.compass}`} />
+    <div
+    id={styles.compassHolder}
+    className={styles.compass}
+    onMouseEnter={() => setCompassHover(true)}
+    onMouseLeave={() => setCompassHover(false)}
+    >
+      <CompassTopLayer className={`${styles.topLayer} ${styles.compass}`} />
+      <CompassTwoLayer className={`${hoverClass('twoLayer')} ${styles.twoLayer} ${styles.compass}`}/>
+      <CompassOneLayer className={`${hoverClass('oneLayer')} ${styles.oneLayer} ${styles.compass}`}/>
+      <CompassZeroLayer className={`${styles.zeroLayer} ${styles.compass}`} />
     </div>
     </>
 )
