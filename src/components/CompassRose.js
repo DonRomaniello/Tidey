2a8b81b2a1fd03bd29a44944823806b0fe1e79f6
Don Romaniello <don.romaniello@gmail.com>
2022-09-02 20:19:27 -0400

    Rotating.

--- src/components/CompassRose.js
+++ src/components/CompassRose.js
@@ -18,11 +18,10 @@ const CompassRose = () => {
   return (
     <>
     <div id={styles.compassHolder} className={styles.compass}>
-      {/* <div className={`${styles.oneLayer} + ${styles.compass}`}>test</div> */}
-       <CompassOneLayer id={styles.oneLayer} className={`${styles.oneLayer} + ${styles.compass}`}/>
-      <CompassTwoLayer id={styles.twoLayer} className={styles.compass}/>
-      <CompassZeroLayer id={styles.zeroLayer} className={styles.compass} />
-      <CompassTopLayer id={styles.topLayer} className={styles.compass}/>
+       <CompassOneLayer className={`${styles.oneLayer} + ${styles.compass}`}/>
+      <CompassTwoLayer className={`${styles.twoLayer} + ${styles.compass}`}/>
+      <CompassZeroLayer className={`${styles.zeroLayer} + ${styles.compass}`} />
+      <CompassTopLayer className={`${styles.topLayer} + ${styles.compass}`}/>
     </div>
     </>
 )
